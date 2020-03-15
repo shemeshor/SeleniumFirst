34410566bb8e9fe4e7ac8ababe91001ce9262274
or.shemesh1@outlook.com <or.shemesh1@outlook.com>
2020-03-15 16:52:28 +0200

    first test

---
+++ SeleniumFirst/Program.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace SeleniumFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com");
+
+
+            IWebElement element = driver.FindElement(By.Name("q"));
+
+            element.SendKeys("executeaotumation");
+
+            driver.Close();
+
+        }
+    }
+}
